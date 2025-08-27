7f9f06a4f52b1628d9da4e6e76bfca384d3b7916
regmysss <kabanigor3@gmail.com>
2025-08-27 22:22:01 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-      {
-        protocol: "https",
-        hostname: "api.open-meteo.com",
-      },
-      {
-        protocol: "https",
-        hostname: "users-notebook.vercel.app",
-      }
-    ],
+    domains: ["randomuser.me", "api.open-meteo.com", "users-notebook.vercel.app"],
   }
 };
 
